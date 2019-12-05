a7713d5fb8dfa6584754445038ee361b656237bf
Jack Gronenthal <jlg6@illinois.edu>
2019-12-04 21:26:45 -0600

    Jack' s edits

---
+++ src/Routes.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import App from './Components/App'
+// import Register from './Components/Register'
+import Schedule from './Components/subComponents/Schedule'
+import ParticleWrapper from './Components/ParticleWrapper'
+/** REVIEW  import Schedule from './Schedule';
+import Events from './Events';
+import Speakers from './Speakers';
+import Sponsors from './Sponsors';
+import Contact from './Contact';
+import About from './About';
+import SponsorUs from './SponsorUs' */
+
+const routes = () => (
+    <div>
+        <Router> {/** FIXME Refer to this later if something is broken */}
+            <div class="mainbody">
+                <ParticleWrapper />
+                {/* <Route exact path="/" component={App} /> */}
+                {/* <Route exact path="/register" component={Register} /> */}
+                <Route exact path="/schedule" component={Schedule} />
+                {/* REVIEW <Route path="/schedule" component={Schedule} />
+                <Route path="/events" component={Events} />
+                <Route path="/speakers" component={Speakers} />
+                <Route path="/sponsors" component={Sponsors} />
+                <Route path="/contact" component={Contact} />
+                <Route path="/about" component={About} />
+                <Route path="/sponsor_us" component={SponsorUs}/> */}
+                {/* <Route path="/pulse_countdown_page" component={App} /> */}
+            </div>
+        </Router>
+    </div>
+);
+
+export default routes;
