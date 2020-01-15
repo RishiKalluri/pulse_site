cdedff71aa334c09a3fbdd27485e05a8765966e1
Rishi Kalluri <rishikalluri@Rishis-MacBook-Pro.local>
2020-01-14 23:38:47 -0600

    last year

--- src/Components/subComponents/directorCards/Card.js
+++ src/Components/subComponents/directorCards/Card.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react'
 import Test from "./test.jpg"
+import FIT1 from "../../media/Future_in_tech_1.JPG"
+import FIT2 from "../../media/Future_in_tech_2.JPG"
+import HW1 from "../../media/Hw_hackathon_1.JPG"
+import HW2 from "../../media/Hw_hackathon_2.JPG"
+import TI from "../../media/TI_CEO_1.JPG"
+import WIT from "../../media/WIT_1.JPG"
+
 import "./Card.css"
 import { Grid, Image } from 'semantic-ui-react'
 import "semantic-ui-css/semantic.min.css";
@@ -13,25 +20,26 @@ class Card extends Component {
           <Grid.Row>
             <Grid.Column>
             <div class="hvrbox">
-              <img src={Test} alt="Mountains" class="hvrbox-layer_bottom"/>
+              <img src={FIT1} alt="Mountains" class="hvrbox-layer_bottom"/>
               <div class="hvrbox-layer_top">
-                <div class="hvrbox-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce porttitor ligula porttitor, lacinia sapien non.</div>
+                <div class="hvrbox-text">
+                Last year we launched our first Future in Focus day with the goal of bringing people working on the cutting edge of the technology industry to students. </div>
               </div>
             </div>
             </Grid.Column>
             <Grid.Column>
             <div class="hvrbox">
-              <img src={Test} alt="Mountains" class="hvrbox-layer_bottom"/>
+              <img src={HW1} alt="Mountains" class="hvrbox-layer_bottom"/>
               <div class="hvrbox-layer_top">
-                <div class="hvrbox-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce porttitor ligula porttitor, lacinia sapien non.</div>
+                <div class="hvrbox-text">Having proven the feasibility of a hardware hackathon and the amazing creativity of students in using provided resources, we are extremely excited to expand the scope of the hardware hackathon event this year. </div>
               </div>
             </div>
             </Grid.Column>
             <Grid.Column>
             <div class="hvrbox">
-              <img src={Test} alt="Mountains" class="hvrbox-layer_bottom"/>
+              <img src={WIT} alt="Mountains" class="hvrbox-layer_bottom"/>
               <div class="hvrbox-layer_top">
-                <div class="hvrbox-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce porttitor ligula porttitor, lacinia sapien non.</div>
+                <div class="hvrbox-text">In just its second iteration, Women in Tech Day (WIT) attracted over 130 students, professors, and industry professionals; a huge success for Pulse this past year.</div>
               </div>
             </div>
             </Grid.Column>
@@ -39,25 +47,25 @@ class Card extends Component {
           <Grid.Row>
             <Grid.Column>
             <div class="hvrbox">
-              <img src={Test} alt="Mountains" class="hvrbox-layer_bottom"/>
+              <img src={FIT2} alt="Mountains" class="hvrbox-layer_bottom"/>
               <div class="hvrbox-layer_top">
-                <div class="hvrbox-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce porttitor ligula porttitor, lacinia sapien non.</div>
+                <div class="hvrbox-text">We brought entrepreneurs, acclaimed professors, and technology leaders in an open forum to discuss their journeys in tech, current work, and perspectives on the future to come.</div>
               </div>
             </div>
             </Grid.Column>
             <Grid.Column>
             <div class="hvrbox">
-              <img src={Test} alt="Mountains" class="hvrbox-layer_bottom"/>
+              <img src={HW2} alt="Mountains" class="hvrbox-layer_bottom"/>
               <div class="hvrbox-layer_top">
-                <div class="hvrbox-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce porttitor ligula porttitor, lacinia sapien non.</div>
+                <div class="hvrbox-text">Pulse 2020 will not only add more sensors, development boards and unique add-ons to be used with projects, but we are also exploring the possibility of having engineers from our sponsors work with student teams!</div>
               </div>
             </div>
             </Grid.Column>
             <Grid.Column>
             <div class="hvrbox">
-              <img src={Test} alt="Mountains" class="hvrbox-layer_bottom"/>
+              <img src={TI} alt="Mountains" class="hvrbox-layer_bottom"/>
               <div class="hvrbox-layer_top">
-                <div class="hvrbox-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce porttitor ligula porttitor, lacinia sapien non.</div>
+                <div class="hvrbox-text">Last year, Riich Templeton, CEO of Texas Instruments spoke about TI’s rich history, and the connections between the University of Illinois, the semiconductor industry, and the bright future of TI.</div>
               </div>
             </div>
             </Grid.Column>
